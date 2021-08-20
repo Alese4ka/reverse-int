64b0d13917d2041efc9df5beec09022039ff8ae2
Alese4ka <alesun4ik@gmail.com>
2021-08-20 22:45:55 +0300

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 module.exports = function reverse (n) {
     let result = '';
     let revN = result + n;
-    
+    revN = Math.abs(revN);
+    revN = result + revN;
     let i = 0;
     while (i < revN.length) {
-      result = `${revN[i]}${result}`;
-      i += 1;
+        result = `${revN[i]}${result}`;
+        i += 1;
     }
     return result;
-  }
+}
   
