2e8eb923856793f6c3156dc1a0de69199ed18bc2
Alese4ka <alesun4ik@gmail.com>
2021-08-20 22:35:53 +0300

    init: create reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    let result = '';
+    let revN = result + n;
+    
+    let i = 0;
+    while (i < revN.length) {
+      result = `${revN[i]}${result}`;
+      i += 1;
+    }
+    return result;
+  }
   
-}
